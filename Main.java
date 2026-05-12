1f71d5c4eb369d5b2a1c141adda9b5d597d65aeb
jjgr29 <jjgr29@umsystem.edu>
2026-05-12 11:04:41 -0500

    initial

--- Main.java
+++ Main.java
@@ -1,28 +1,55 @@
-import java.util.Random;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Welcome to classes!");
+
+        //define file values
+        ArrayList<Person> people = new ArrayList<>();
         
-        //CREATE A PERSON OBJECT USING OVERLOADED CONSTRUCTOR
-        Person p1 = new Person("Einstien","Albert", 144);
+        //CREATE A STUDENT OBJECT USING OVERLOADED CONSTRUCTOR
+        Student s1 = new Student('S', "Mahomes","Patrick", 29, 3.5);
+        System.out.println(s1);
 
-        //CREATE A PERSON OBJECT USING DEFAULT CONSTRUCTOR
-
-
-
+        //CREATE A STUDENT OBJECT USING DEFAULT CONSTRUCTOR
+        Student s2 = new Student();
+        s2.setType('S');
+        s2.setLName("Kelce");
+        s2.setFName("Travis");
+        s2.setAge(35);
+        s2.setGPA(3.4);
+        System.out.println(s2);
+        Person px = new Person("Bubba", "Wubba",15);
         //PRINT OBJECTS 
-         System.out.println(p1.printPerson());
+         System.out.print(s1);
+         System.out.print(s2);
 
+        people.add(s1);
+        people.add(s2);
+        people.add(px);
+
+        for (Person p : people){
+            System.out.print(p);
+        }
+
+        //testing Apple class
+        System.out.println("\n\nTESTING APPLE CLASS");
+        Apple a = new Apple();
+        System.out.println(a + "\n");
+        
+        GoldenDelicious b = new GoldenDelicious(7);
+        System.out.println(b + "\n");
+
+        Apple c = new GoldenDelicious(8);
+        System.out.println(c + "\n");
+
+        Apple d = new GoldenDelicious();
+        System.out.println(d + "\n");
+ 
         try {
-            //CREATE AN ARRAYLIST OF PERSON OBJECTS
-            ArrayList<Person> people = new ArrayList<>();
-
             File inFile = new File("people.txt");
             Scanner scanner = new Scanner(inFile);
 
@@ -31,62 +58,82 @@ public class Main {
                 System.out.println("Input is not valid");
             }
             
-            //CREATE A PRINTWRITER OBJECT & WRITE TO THE OUTPUT FILE
+            System.out.println("\nPRINT ALL PEOPLE USING OBJECT PRINTPERSON");
             for (Person p : people){
-                System.out.printf(p.printPerson());
+                System.out.print(p);
             }
-            System.out.println("Total People: " + Person.totalPeople);
+            System.out.println("Total People:   " + Person.totalPeople);
+            System.out.println("Total Students: " + Student.totalStudent);
+
+            System.out.println("\nPRINT ALL PEOPLE USING GETTERS");
+            for (Person p : people){
+                System.out.print(p);
+            }
+
+            System.out.println("\nPRINT ONLY STUDENTS");
+            //using instanceof to print only
+            for (Person p : people){
+                if (p instanceof Student)
+                    System.out.print(p);
+            }
             scanner.close();
         }
 
         catch (FileNotFoundException e){
             System.out.println("Unable to open file");
-            return;
         }
-
-        
-        //CHECKING UNDERSTANDING
-        Other f = new Other();
-        System.out.println(f.i);   
-        System.out.println(f.s);     
-        f.imethod();
-        //f.smethod();
-        Other.smethod();
-        
+       
     }
 
     public static int loadArrayList(ArrayList<Person> people, Scanner input){
 
-        String inputLine;
-        char t;
+        String inputLine, f, l;
         int a;
-        String f, l;
-
+        double g;
  
         while (input.hasNextLine()){
             //get the next line of input from the file
             inputLine = input.nextLine();
             String[] tokens = inputLine.split(",");
+
             //check that the number of tokens includes row & colum
-            if (tokens.length < 4){
+            if (tokens[0].equals("P") && tokens.length < 4){
                 return -1;
             } 
-            f = tokens[1];
-            l = tokens[2];
+            if (tokens[0].equals("S") && tokens.length < 5){
+                return -1;
+            } 
+            f = tokens[2];
+            l = tokens[1];
 
-            try{
-                a = Integer.parseInt(tokens[3]);
-                Person newPerson = new Person(f,l,a);
-                people.add(newPerson);
-
+            if (tokens[0].equals("P")){
+                try{
+                    a = Integer.parseInt(tokens[3]);
+                    Person newPerson = new Person(f,l,a);
+                    people.add(newPerson);
+                }
+                catch(NumberFormatException e){
+                    System.out.println("Error in the line: " + inputLine);
+                }
             }
-            catch(NumberFormatException e){
-                System.out.println("Error in the line: " + inputLine);
+            else if (tokens[0].equals("S")){
+                try{
+                    a = Integer.parseInt(tokens[3]);
+                    g = Double.parseDouble(tokens[4]);
+                    //Student newPerson = new Student('S',f,l,a,g);
+                    //people.add(newPerson);
+                    Person newPerson2 = new Student('S',f,l,a,g);
+                    people.add(newPerson2);
+                }
+                catch(NumberFormatException e){
+                    System.out.println("Error in the line: " + inputLine);
+                }
             }
 
        }
                    
        return 1;
     }
+      
 }
 
